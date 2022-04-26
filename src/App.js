0c27f43914123c8e4878840bc19c1d2a484c7237
Giuseppe <giuseppeuhlmann@gmail.com>
2022-04-26 16:10:47 -0300

    feat: adicionando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as api from './services/api';
+
 import './App.css';
 
 function App() {
+  api.getCategories().then((categories) => { console.log(categories); });
+  api.getProductsFromCategoryAndQuery('MLB5672').then((categoriesAndQuery) => {
+    console.log(categoriesAndQuery);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Teste
     </div>
   );
 }
