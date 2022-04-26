3b4881015778b41bc17b3416d39d9611333335c1
Gisele <gisele.viedenhelfen@gmail.com>
2022-04-26 16:43:00 -0300

    altera nome da pasta para pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
