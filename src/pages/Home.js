8d5696855dbbf29c10bdc34fdd659cbe7ece0732
Thayara Siqueira <thay.siqu@gmail.com>
2022-04-26 15:08:38 -0600

    requisito 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import Card from '../components/Card';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      productList: [],
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+handleSearch = async () => {
+    const { search } = this.state;
+    const listProducts = await getProductsFromCategoryAndQuery(search);
+    const { results } = listProducts
+    this.setState({ productList: results });
+  }
+
   render() {
+    const { productList } = this.state;
     return (
       <div>
         <label htmlFor="input-pesquisa">
           <input
+            data-testid="query-input"
             id="input-pesquisa"
             type="text"
-            name="input-pesquisa"
+            name="search"
+            onChange={ this.handleInputChange }
           />
         </label>
         <p
@@ -17,7 +43,16 @@ class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <ul />
+        <button data-testid="query-button" type="button" onClick={ this.handleSearch }>Buscar</button>
+        { productList.map((products) =>(
+          <Card
+          title={products.title}
+          price={products.price}
+          image={products.thumbnail}
+          />
+        ))}
       </div>);
   }
 }
+
 export default Home;
